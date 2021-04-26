d0e8e6b27b6b913b897838c588287b97db5700eb
Sanket Ughadmathe <59117184+sanketughadmathe@users.noreply.github.com>
2021-04-26 09:40:58 +0530

    first commit

---
+++ android/app/src/main/java/com/sanket/magic_8_ball/MainActivity.java
@@ -0,0 +1,6 @@
+package com.sanket.magic_8_ball;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    Magic8Ball(),
+  );
+}
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask me Anything'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
